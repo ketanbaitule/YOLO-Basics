d0b09472d6fee6e4349196558e8e6be1990d65b8
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-03-03 06:06:46 +0000

    Add YOLO model

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from ultralytics import YOLO
+from PIL import Image
 
 @st.cache_resource
 def get_model():
@@ -12,7 +13,8 @@ st.title("YOLO model Basic")
 
 uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=False, type=["png", "jpg", "jpeg"])
 if uploaded_file is not None:
-    st.image(uploaded_file)
-    res = model.predict(image, conf=conf)
-    res_plotted = res[0].plot()
-    st.image(res_plotted)
+    st.image(uploaded_file, caption="Original Image")
+    img = Image.open(uploaded_file)
+    res = model.predict(img)
+    res_plotted = res[0].plot()[:, :, ::-1]
+    st.image(res_plotted, "Result Image")
