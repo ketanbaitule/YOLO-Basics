affddb656bb88c423e4d926881bd64257d83a2fd
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-03-03 06:10:13 +0000

    Improved Layout

--- App.py
+++ App.py
@@ -13,8 +13,13 @@ st.title("YOLO model Basic")
 
 uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=False, type=["png", "jpg", "jpeg"])
 if uploaded_file is not None:
-    st.image(uploaded_file, caption="Original Image")
     img = Image.open(uploaded_file)
     res = model.predict(img)
     res_plotted = res[0].plot()[:, :, ::-1]
-    st.image(res_plotted, "Result Image")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(uploaded_file, caption="Original Image")
+    
+    with col2:
+        st.image(res_plotted, "Result Image")
